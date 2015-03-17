2b4e521ac583387fbe20a5bc80d5efb5c523d213
Monico (collaborare) Moreno <cdnadmin@collaborare.net>
2015-03-16 23:00:56 -0500

    added quick files/updates for mean stack job listings

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res) {
+  res.render('about');
+});
+
 module.exports = router;
