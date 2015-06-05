35380b3e1d1f2d80fb7454450d51e4f2ff1e0937
Monico Moreno <monico_moreno@collaborare.net>
2015-06-04 20:07:12 -0500

    had to fix chevron toggle logic after previous, slight layout changes

--- public/javascripts/index.bs.js
+++ public/javascripts/index.bs.js
@@ -1,5 +1,5 @@
 	$(document).ready(function () {
-		$('a[data-toggle="collapse"]').click(function () {
+		$('h3.panel-title').click(function () {
 		$(this).find('span.toggle-icon').toggleClass('glyphicon-chevron-up glyphicon-chevron-down');
 		});
 	});
