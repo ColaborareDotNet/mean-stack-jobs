e49cd6b22a51fce238bcb494da816c419b1d2245
Monico Moreno <monico_moreno@collaborare.net>
2015-06-05 21:58:17 -0500

    added functionality to scroll into job listing and expand details; moved dependencies around for this in interim, till we get requirejs in place

--- public/javascripts/index.ng.js
+++ public/javascripts/index.ng.js
@@ -1,7 +1,38 @@
 
 (function(){//IIFE BEGIN
+var indexNgApp = angular.module('indexNgApp', ['ngRoute']);
 
-var indexNgApp = angular.module('indexNgApp', []);
+indexNgApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider.when('/listing/:jobId', {
+    template: '',
+    controller: function($scope, $routeParams, $location, $timeout, $anchorScroll){
+      $scope.elem = angular.element("a[href='" + "#" + $routeParams.jobId + "-panel']");
+
+      $timeout(function(){
+            /*
+              Why in here? See (6/5/2015): https://docs.angularjs.org/error/$rootScope/inprog/#triggering-events-programmatically
+            */
+            $scope.elem.trigger('click');
+          },
+        0, false);
+
+      var theJobId = $routeParams.jobId;
+
+      $timeout(function(){
+          /*
+            Why in here? See (6/5/2015): https://docs.angularjs.org/error/$rootScope/inprog/#triggering-events-programmatically
+          */
+          $location.path('/').hash(theJobId);
+          $anchorScroll();
+        }, 0, false
+
+      );
+    } 
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+}]);
 
 indexNgApp.controller('ListingsController', ['$log', function($log){
   var self = this;
