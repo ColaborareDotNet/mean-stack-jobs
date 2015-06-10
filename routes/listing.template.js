4759e4efa0b6e8c649ecc8acc3802809fc6ec79e
Monico Moreno <monico_moreno@collaborare.net>
2015-06-10 17:03:29 -0500

    add template for listings (till we get mongodb in play

---
+++ routes/listing.template.js
@@ -0,0 +1,32 @@
+{
+        jobId: 'jb-yyyy-mm-dd-hhmm',
+        jobTitle: '',
+        degree: '',
+        level: '',
+        hireType: '',
+        skills: [
+            '',
+        ],
+        tools: [],
+        responsibilities: [],
+        briefDescr: '',
+        teamStructureExpertise: null,
+        projectWorkflow: null,
+        recruiter: 'n/a',
+        hiringCompany: null,
+        compensationRange: '',
+        locationOppWork: '',
+        locationCompanyOffice: '',
+        benefitsGeneral: null,
+
+        employmentEligibility: null,
+
+        vettingProcess: null,
+        hireTimeframeValue: null,
+        postingDate: new Date('6/5/2015 22:25'),
+        hireDate: '',
+        /* if null or min AND there is a closingDate, indicates opp was never filled */
+        closingDate: '',
+        companyWebSite: null,
+        notes: null
+}
