d839162c8a2c32b32a8f6d0d4934f8d15fedcd70
Monico Moreno <monico_moreno@collaborare.net>
2015-06-14 22:37:55 -0500

    adjusted title-panel handler to toggle icon based on content panel's 'in' status

--- public/javascripts/index.bs.js
+++ public/javascripts/index.bs.js
@@ -1,7 +1,25 @@
-	$(document).ready(function () {
-		$('h3.panel-title').click(function () {
-		$(this).find('span.toggle-icon').toggleClass('glyphicon-chevron-up glyphicon-chevron-down');
-		});
+	$(document).ready(function() {
+	  setHandlerForClickedJobTitlePanel();
 	});
 
 
+	function setHandlerForClickedJobTitlePanel() {
+	  $("h3.panel-title").click(function() {
+
+	    //use parent elem to find sib that needs icon toggling
+	    var parent = $(this).parents("div.panel.panel-primary");
+	    var sib = parent.find("div.panel-collapse.collapse");
+	    var elemToToggleIcon = $(this).find("span.toggle-icon");
+
+	    //if sib elem has class "in", meaning it's expanded (and not collapsed), and we're about to collapse it
+	    if (sib.attr("class").split(" ").indexOf("in") >= 0) {
+	      elemToToggleIcon.removeClass("glyphicon-chevron-up");
+	      elemToToggleIcon.addClass("glyphicon-chevron-down");
+	    } else {
+	      elemToToggleIcon.removeClass("glyphicon-chevron-down");
+	      elemToToggleIcon.addClass("glyphicon-chevron-up");
+	    }
+
+	  });
+
+	}
